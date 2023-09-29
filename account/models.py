9dab9d1a5ffd72cd8751fb3a732bee7c84c7feb0
Dimtiris-Mourtzilas <dimitirs.mourtzilas1999@gmail.com>
2023-09-29 15:59:44 +0300

    initial commits

---
+++ account/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Client(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+    email = models.EmailField()
+    password = models.TextField(max_length=255)
+
+
+class Account(models.Model):
+
+    date_created = models.DateField()
+    balance = models.FloatField()
+    client = models.ForeignKey(Client,on_delete=models.CASCADE)
---
+++ account/views.py
@@ -0,0 +1,36 @@
+import logging
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from account import models
+from account.models import Client
+
+logger = logging.getLogger(__name__)
+
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        client = Client(name=name,surname=surname,email=email,password=password)
+        client.save()
+        return render(request,'index.html',{'success_message':'User registered succesfully'})
+    return render(request, 'register.html')
+
+
+def authenticate(request):
+    try:
+        user = models.Client.objects.get(email=request.POST.get('email'),password=request.POST.get('password'))
+        return render(request, 'index.html', {'success_message': 'User exists'})
+
+    except ObjectDoesNotExist:
+        return render(request,'index.html',{'error_message':'User does not exist'})
